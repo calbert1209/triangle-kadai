567fedf86288d6f497470781fe6517cb4d0439a6
Christian Albert <c_albert@yahoo.com>
2015-11-24 20:28:03 +0900

    alter ENG responses, add error msg re: parameters
    alter ENG responses - valid triangles return [true, JPN text, ENG text]
    
    add error msg re: parameters - executing script from command line with more or less than three parameters results in an error msg.

--- triangle.rb
+++ triangle.rb
@@ -12,17 +12,19 @@ class Triangle
     
     #check if triangle
     if args[0] + args[1] <= args[2]
-      return [false, "三角形じゃないです!＞＜"]
+      return [false, "三角形じゃないです!＞＜", "(invalid)"]
     elsif args.max == args.min
-      return [true, "正三角形(equilateral triangle)ですね！"]
+      return [true, "正三角形ですね！", "(equilateral triangle)"]
     elsif args[0] == args[1] || args[1] == args[2] || args[2] == args[0]
-      return [true, "二等辺三角形(isoceles triangle)ですね！"]
+      return [true, "二等辺三角形ですね！", "(isoceles triangle)"]
     else
-      return [true, "不等辺三角形(scalene triangle)ですね！"]
+      return [true, "不等辺三角形ですね！" ,"(scalene triangle)"]
     end
   end
 end
 
 if ARGV.count == 3
   puts Triangle.catagorize()
+elsif ARGV.count > 3 || ARGV.count < 3
+  puts "please include 3 sides: '$ ruby triangle.rb <side> <side> <side>'"
 end
